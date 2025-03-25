410f2983b917d63497cd3c29255a83c058d93750
Rob <robveijk@gmail.com>
2025-03-25 10:21:48 +0100

    Implement photos view

--- concert/views.py
+++ concert/views.py
@@ -28,9 +28,17 @@ def songs(request):
 
 
 def photos(request):
-    # photos = []
-    # return render(request, "photos.html", {"photos": photos})
-    pass
+    # TODO: Get pictures from API
+    photos = [{
+        "id": 1,
+        "pic_url": "http://dummyimage.com/136x100.png/5fa2dd/ffffff",
+        "event_country": "United States",
+        "event_state": "District of Columbia",
+        "event_city": "Washington",
+        "event_date": "11/16/2022"
+    }]
+    return render(request, "photos.html", {"photos": photos})
+    
 
 def login_view(request):
     pass
